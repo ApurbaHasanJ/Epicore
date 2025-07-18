574634eaf777c02286da7e6168fe39040953dc1c
ApurbaHasanJ <jx.apurba8881@gmail.com>
2025-07-18 20:58:18 +0600

    :recycle: footer has been updated

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,17 +1,9 @@
-"use client"
-
-import { motion } from "framer-motion"
+"use client";
 
 export default function Footer() {
   return (
-    <motion.footer
-      initial={{ y: 100, opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
-      viewport={{ once: true, amount: 0.3 }}
-      transition={{ duration: 0.5 }}
-      className="py-8 -mb-2 bg-secondary text-white-custom text-center px-4 md:px-8"
-    >
+    <footer className="py-8 -mb-2 bg-secondary text-white-custom text-center px-4 md:px-8">
       <p className="text-sm">Sample text. Click to select the Text Element.</p>
-    </motion.footer>
-  )
+    </footer>
+  );
 }
